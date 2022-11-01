80fb6aa9a767f6be73f4431522ccd95d2b3b8887
RST <rstkachev@gmail.com>
2022-11-01 22:22:53 +0300

    CarShowRoomApp

---
+++ app/src/main/java/com/example/carshowroom/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.carshowroom
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val homeIntent = Intent(this@SplashScreen, MainActivity::class.java)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(homeIntent)
+            finish()
+        }, SPLASH_TIME_OUT.toLong())
+    }
+
+    companion object {
+        const val SPLASH_TIME_OUT = 2000
+    }
+}
